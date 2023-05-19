2fa83c520bc0596a0d2cc35506b4ccce4223128a
phenyomalele <phenyomalele@gmail.com>
2023-05-19 12:37:25 +0200

    My first iOS tutorial

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by DA MAC M1 117 on 2023/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
